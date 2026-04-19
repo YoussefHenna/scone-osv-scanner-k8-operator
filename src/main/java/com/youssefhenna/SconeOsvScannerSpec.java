68b8f461aa6ddc86d612c4a61fe24e1a3deca59b
Youssef Henna <youssef.hisham14@gmail.com>
2026-04-19 14:13:11 +0200

    add spec fields

--- src/main/java/com/youssefhenna/SconeOsvScannerSpec.java
+++ src/main/java/com/youssefhenna/SconeOsvScannerSpec.java
@@ -1,4 +1,53 @@
 package com.youssefhenna;
 
+import com.youssefhenna.model.RegistryCredentials;
+import com.youssefhenna.model.ResourceRef;
+
 public class SconeOsvScannerSpec {
+    private String registryUrl;
+    private String registryRepository;
+    private RegistryCredentials registryCredentials;
+    private String casAddress;
+    private ResourceRef casPolicy;
+
+    public String getRegistryUrl() {
+        return registryUrl;
+    }
+
+    public void setRegistryUrl(String registryUrl) {
+        this.registryUrl = registryUrl;
+    }
+
+    public String getRegistryRepository() {
+        return registryRepository;
+    }
+
+    public void setRegistryRepository(String registryRepository) {
+        this.registryRepository = registryRepository;
+    }
+
+    public RegistryCredentials getRegistryCredentials() {
+        return registryCredentials;
+    }
+
+    public void setRegistryCredentials(RegistryCredentials registryCredentials) {
+        this.registryCredentials = registryCredentials;
+    }
+
+    public String getCasAddress() {
+        return casAddress;
+    }
+
+    public void setCasAddress(String casAddress) {
+        this.casAddress = casAddress;
+    }
+
+    public ResourceRef getCasPolicy() {
+        return casPolicy;
+    }
+
+    public void setCasPolicy(ResourceRef casPolicy) {
+        this.casPolicy = casPolicy;
+    }
+
 }
---
+++ src/main/java/com/youssefhenna/model/RegistryCredentials.java
@@ -0,0 +1,14 @@
+package com.youssefhenna.model;
+
+
+public class RegistryCredentials {
+    private SecretRef secretRef;
+
+    public SecretRef getSecretRef() {
+        return secretRef;
+    }
+
+    public void setSecretRef(SecretRef secretRef) {
+        this.secretRef = secretRef;
+    }
+}
